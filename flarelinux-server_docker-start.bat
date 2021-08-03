638e8c083d6d7d666f987941eb46e07421ae46fd
Andrew Caya <andrewscaya@yahoo.ca>
2021-08-02 20:35:32 -0400

    2021-08-02 AC: Fixes a typo in the Batch scripts.

--- flarelinux-server_docker-start.bat
+++ flarelinux-server_docker-start.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 docker run -it --rm -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.0.0-rc1 flare --coston
-ECHO Flare server succesfully exited
+ECHO Flare server successfully exited
 pause
