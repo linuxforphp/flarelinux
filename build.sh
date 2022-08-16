0cbbb03a7da45b5e12a6e1d10041132a2c289ab3
Andrew Caya <andrewscaya@yahoo.ca>
2022-08-16 19:00:53 -0400

    2022-08-16 AC: Updates the Flare Linux version number.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 cd flarelinux-image &&
-docker build . -t localhost:5000/flarelinux:1.0.0
+docker build . -t localhost:5000/flarelinux:1.0.1
--- flarelinux-server_docker-start.sh
+++ flarelinux-server_docker-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.0.0 flare --coston
+docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.0.1 flare --coston
 echo "\nFlare server successfully exited\n"
 exit 0
