65c178e8d516792df83213c1bb8cab05606fd3e4
Andrew Caya <andrewscaya@yahoo.ca>
2023-11-20 16:18:37 -0500

    2023-11-20 AC: Updates the Flare node version to 1.7.1806.

--- flarelinux-server_docker-start.sh
+++ flarelinux-server_docker-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.1.2 flare --coston
+docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.1.2-1.7.1806 flare --flare-coston2
 echo "\nFlare server successfully exited\n"
 exit 0
