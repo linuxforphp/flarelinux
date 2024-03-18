ffe518f7e95e17d5d4e57510d8cbf2bc38c3921b
Andrew Caya <andrewscaya@yahoo.ca>
2024-03-18 12:44:18 -0400

    2024-03-18 AC: Updates the FLR node version to 1.7.1807, and the SGB node version to 0.6.5.

--- flarelinux-server_docker-start.bat
+++ flarelinux-server_docker-start.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.1.2-1.7.1806 flare --flare-coston2
+docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.1.2-1.7.1807-0.6.5 flare --flare-coston2
 ECHO Flare server successfully exited
 pause
