6659f223c3fee4496739ce0c4acf35f419ed0d9b
Andrew Caya <andrewscaya@yahoo.ca>
2025-11-05 18:02:25 -0500

    2025-11-05 AC: Bumps the version number.

--- flarelinux-server_docker-start.sh
+++ flarelinux-server_docker-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.2.0-1.11.0 flare --flare-coston2
+docker run -it --rm -e FLARE_BIND_ADDRESS=0.0.0.0 -p 9650:9650 --name my_flare_server asclinux/flarelinux:1.2.0-1.11.13 flare --flare-coston2
 echo "\nFlare server successfully exited\n"
 exit 0
